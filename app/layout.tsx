dcf880f1338a0146e809626d5b67ae4bb021bd0a
Valeryia Chyzhova <lera.chizh111@gmail.com>
2026-03-23 22:01:01 +0100

    analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -81,6 +85,8 @@ export default function RootLayout({
             </main>
             <Footer />
           </div>
+          <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ConvexClerkProvider >
